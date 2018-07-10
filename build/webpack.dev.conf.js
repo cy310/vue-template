4fca11cb35b43637350bcb9bbfda134829a1d990
Yang.C <str_cy@163.com>
2018-07-10 20:05:19 +0800

    remove

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -24,17 +24,12 @@ var devWebpackConfig = merge(baseWebpackConfig, {
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin(),
-        // Webpack压缩代码的时候，React官方提供的代码已经是合并的, 可以通过以下插件优化
         new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: '"production"'
             }
         }),
-        /* new webpack.optimize.UglifyJsPlugin({
-             compress: {
-                 warnings: false
-             }
-         }),*/
+
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './index.html',
@@ -47,66 +42,6 @@ var devWebpackConfig = merge(baseWebpackConfig, {
         }),
         new ExtractTextPlugin("[name].css"),
     ]
-})
-
-    // output: {
-    //     path: path.join(__dirname, 'coupon/'),
-    //     filename: '[name].js',
-    // },
-    // module: {
-    //     loaders: [{
-    //         test: /\.vue$/,
-    //         loader: 'vue-loader',
-    //     },
-    //         {
-    //             test: /\.js$/,
-    //             loader: 'babel-loader',
-    //             exclude: /node_modules/,
-    //             query: {
-    //                 presets: ['env'] // es2015 处理 ES6 语法
-    //             }
-    //         },
-    //         {
-    //             test: /\.(png|jpg|gif)$/,
-    //             loader: 'url-loader',
-    //             options: {
-    //                 limit: 8192
-    //             }
-    //         },
-    //         {
-    //             test: /\.css$/,
-    //             use: ExtractTextPlugin.extract({ fallback: "style-loader", use: ["css-loader"] })
-    //         }
-    //     ],
-    // },
-
-
-    // plugins: [
-    //     new webpack.HotModuleReplacementPlugin(),
-    //     new webpack.NoEmitOnErrorsPlugin(),
-    //     // Webpack压缩代码的时候，React官方提供的代码已经是合并的, 可以通过以下插件优化
-    //     new webpack.DefinePlugin({
-    //         'process.env': {
-    //             NODE_ENV: '"production"'
-    //         }
-    //     }),
-    //     /* new webpack.optimize.UglifyJsPlugin({
-    //          compress: {
-    //              warnings: false
-    //          }
-    //      }),*/
-    //     new HtmlwebpackPlugin({
-    //         filename: 'index.html',
-    //         template: './index.html',
-    //         // template: './src/h5.html',
-    //         minify: { //压缩HTML文件
-    //             removeComments: true, //移除HTML中的注释
-    //             collapseWhitespace: true //删除空白符与换行符
-    //         },
-    //         hash: true,
-    //     }),
-    //     new ExtractTextPlugin("[name].css"),
-    // ]
-
+});
 
 module.exports = devWebpackConfig;
